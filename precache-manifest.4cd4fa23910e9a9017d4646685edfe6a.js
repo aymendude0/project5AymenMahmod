86b7c9d3aca34578875e1ce2dd7944f1acae76aa
Aymen Mahmod <aymenm@live.com>
2020-08-13 20:59:20 -0400

    Updates

---
+++ precache-manifest.4cd4fa23910e9a9017d4646685edfe6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e33235704e967ee9b4cd3ac777f085bb",
+    "url": "/project5AymenMahmod/index.html"
+  },
+  {
+    "revision": "6808028b8d6f02f8eb90",
+    "url": "/project5AymenMahmod/static/css/main.8a54eeee.chunk.css"
+  },
+  {
+    "revision": "3f3c507a8242339dd947",
+    "url": "/project5AymenMahmod/static/js/2.34aa92f4.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/project5AymenMahmod/static/js/2.34aa92f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6808028b8d6f02f8eb90",
+    "url": "/project5AymenMahmod/static/js/main.5020e7af.chunk.js"
+  },
+  {
+    "revision": "836d81ea12706e376591",
+    "url": "/project5AymenMahmod/static/js/runtime-main.1786c172.js"
+  },
+  {
+    "revision": "a485c0ea5cfb26eb9f813f7cf84d1a0f",
+    "url": "/project5AymenMahmod/static/media/0d3e4a84ba9f77eef545736731c9e2a8.a485c0ea.jpg"
+  }
+]);
